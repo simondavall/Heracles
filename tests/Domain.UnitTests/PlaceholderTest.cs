7da9ec38a30136938a69b11b15b348f4fc3f6b77
Simon Da Vall <simon.davall@gmail.com>
2021-10-31 11:18:31 +0000

    Initial Commit

---
+++ tests/Domain.UnitTests/PlaceholderTest.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Heracles.Domain.UnitTests
+{
+    class PlaceholderTest
+    {
+        [Test]
+        public void Place_Holder_Test()
+        {
+            true.Should().BeTrue("Placeholder Test");
+        }
+    }
+}
---
+++ tests/Web.UnitTests/PlaceholderTest.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Heracles.Web.UnitTests
+{
+    class PlaceholderTest
+    {
+        [Test]
+        public void Place_Holder_Test()
+        {
+            true.Should().BeTrue("Placeholder Test");
+        }
+    }
+}
