b8452af32041a4a3018ecbc7a9e99d4c607c5b84
Simon Da Vall <simon.davall@gmail.com>
2026-09-14 12:04:15 +0100

    hcl-46: Added login requirement via Soteria

--- src/Heracles.Web/Program.cs
+++ src/Heracles.Web/Program.cs
@@ -1,8 +1,26 @@
+using DotNetEnv;
 using Heracles.Application;
 using Heracles.Infrastructure;
 using Heracles.Web.Components;
+using Heracles.Web.Components.Features.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+_ = bool.TryParse(Environment.GetEnvironmentVariable("HERACLES_LOCAL_EXECUTION"), out var isLocalExecution);
+if (isLocalExecution) {
+    Env
+        .NoClobber()
+        .TraversePath()
+        .Load();
+
+    builder.Configuration.AddEnvironmentVariables();
+
+    builder.WebHost.UseStaticWebAssets();
+}
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
@@ -10,6 +28,8 @@ builder.Services.AddApplication();
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddHeraclesAuthentication(builder.Configuration);
 
 var app = builder.Build();
 
@@ -29,10 +49,46 @@ app.MapControllerRoute(
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseAntiforgery();
 
-app.MapStaticAssets();
+app.MapStaticAssets()
+    .Add(endpointBuilder => endpointBuilder.Metadata.Add(new AllowAnonymousAttribute()));
+
+app
+    .MapGet(
+        "/authentication/login",
+        (string? returnUrl) => {
+            var properties = new AuthenticationProperties { RedirectUri = IsLocalReturnUrl(returnUrl) ? returnUrl! : "/" };
+            return Results.Challenge(properties, [OpenIdConnectDefaults.AuthenticationScheme]);
+        })
+    .AllowAnonymous();
+
+app
+    .MapGet(
+        "/authentication/logout",
+        () => {
+            var properties = new AuthenticationProperties { RedirectUri = "/" };
+            return Results.SignOut(properties, [CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme]);
+        })
+    .AllowAnonymous();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
+return;
+
+// For security: Make sure the return url is a local address
+// and not some random other (potentially dangerous) address
+static bool IsLocalReturnUrl(string? returnUrl)
+{
+    if (string.IsNullOrWhiteSpace(returnUrl))
+        return false;
+
+    return returnUrl.StartsWith('/')
+           && !returnUrl.StartsWith("//")
+           && !returnUrl.StartsWith("/\\");
+}
