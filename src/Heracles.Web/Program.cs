df5ed62ec2140e47f79ed7591a8b3bc905327ab2
Simon Da Vall <simon.davall@gmail.com>
2026-09-16 20:52:52 +0100

    hcl-51: Separated Legacy Identity Infrastructure and Integrated Soteria Authentication

--- src/Heracles.Web/Program.cs
+++ src/Heracles.Web/Program.cs
@@ -1,12 +1,28 @@
+using DotNetEnv;
 using Heracles.Application;
 using Heracles.Infrastructure;
 using Heracles.Web.Components;
+using Heracles.Web.Components.Features.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+_ = bool.TryParse(Environment.GetEnvironmentVariable("HERACLES_LOCAL_EXECUTION"), out var isLocalExecution);
+if (isLocalExecution) {
+    Env.NoClobber()
+        .TraversePath()
+        .Load();
+
+    builder.Configuration.AddEnvironmentVariables();
+
+    builder.WebHost.UseStaticWebAssets();
+}
+
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
+
+builder.Services.AddHeraclesAuthentication(builder.Configuration);
 
 builder.Services.AddMudServices();
 
@@ -15,17 +31,24 @@ builder.Services.AddRazorComponents()
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
+if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseAntiforgery();
 
-app.MapStaticAssets();
+app.MapStaticAssets()
+    .Add(endpointBuilder => endpointBuilder.Metadata.Add(new AllowAnonymousAttribute()));
+
+app.UseHeraclesAuthentication();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
