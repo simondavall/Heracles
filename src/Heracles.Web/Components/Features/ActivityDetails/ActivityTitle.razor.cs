f9676cf69bde51dc5deca4406df88c6e21f7aa13
Simon Da Vall <simon.davall@gmail.com>
2026-09-23 13:12:23 +0100

    hcl-60: Created Activity Title Component

---
+++ src/Heracles.Web/Components/Features/ActivityDetails/ActivityTitle.razor.cs
@@ -0,0 +1,28 @@
+﻿using Heracles.Application.Enums;
+using Heracles.Application.TrackAggregate;
+using Microsoft.AspNetCore.Components;
+
+namespace Heracles.Web.Components.Features.ActivityDetails;
+
+public partial class ActivityTitle
+{
+    [Parameter]
+    public Track Track { get; set; } = null!;
+
+    private string ImagePath => Track.ActivityType switch
+    {
+        ActivityType.Cycling => "/images/icon-cycling.png",
+        _ => "/images/icon-running.png"
+    };
+
+    private string ActivityTitleText => Track.ActivityType switch
+    {
+        ActivityType.Running => "Run",
+        ActivityType.Cycling => "Bike Ride",
+        _ => string.Empty
+    };
+
+    private string Title => $"{Track.Time.DayOfWeek} {ActivityTitleText}";
+
+    private string Date => Track.Time.ToString("MMM dd, yyyy - HH:mm");
+}
---
+++ src/Heracles.Web/Components/Pages/Home.razor.cs
@@ -0,0 +1,23 @@
+﻿using Heracles.Application.Interfaces;
+using Heracles.Application.TrackAggregate;
+using Microsoft.AspNetCore.Components;
+
+namespace Heracles.Web.Components.Pages;
+
+public partial class Home
+{
+    [Inject]
+    private IActivityService ActivityService { get; set; } = null!;
+
+    [Parameter]
+    public Guid? ActivityId { get; set; }
+
+    private Track? _activity;
+
+    protected override async Task OnParametersSetAsync()
+    {
+        _activity = ActivityId.HasValue
+            ? await ActivityService.GetActivityAsync(ActivityId.Value)
+            : null;
+    }
+}
